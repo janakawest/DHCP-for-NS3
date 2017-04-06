eafc0889817d7ac630a58c10892f9baee400e7fb
Janaka Wijekoon <janaka@west.sd.keio.ac.jp>
2017-04-06 08:52:05 +0530

    First Commit

--- model/dhcp-header.cc
+++ model/dhcp-header.cc
@@ -77,5 +77,122 @@ namespace ns3 {
 						m_optionDNS.size () + 1 /*size of the DNS server IP and the termination*/ +
 						m_optionDNS2.size () + 1 /*size of the DNS server2 IP and the termination */
 				   ); 
+  }
+  void 
+  DHCPMessage::Serialize (Buffer::Iterator start) const
+  {
+    Buffer::Iterator i = start;
+
+		i.WriteU16 (m_opcode);
+		i.WriteU16 (m_hType);
+		i.WriteU16 (m_hLen);
+		i.WriteHtonU32 (m_xId);
+		i.WriteHtonU16 (m_eCs);
+		i.WriteHtonU16 (m_flags);
+		
+		// This method has been used to send variable length strings
+		// Client's IP address
+	  i.WriteU16 (m_ciaddr.size () + 1);
+		i.Write ((uint8_t*) m_ciaddr.c_str (), m_ciaddr.size () + 1);
+		// Your Ip address IP address
+	  i.WriteU16 (m_yiaddr.size () + 1);
+		i.Write ((uint8_t*) m_yiaddr.c_str (), m_yiaddr.size () + 1);
+		// Next Servers IP address
+	  i.WriteU16 (m_siaddr.size () + 1);
+		i.Write ((uint8_t*) m_siaddr.c_str (), m_siaddr.size () + 1);
+		// Relay Agent's IP address
+	  i.WriteU16 (m_giaddr.size () + 1);
+		i.Write ((uint8_t*) m_giaddr.c_str (), m_giaddr.size () + 1);
+		// MAC address
+	  i.WriteU16 (m_chaddr.size () + 1);
+		i.Write ((uint8_t*) m_chaddr.c_str (), m_chaddr.size () + 1);
+		// Gateway IP address
+	  i.WriteU16 (m_optionGateway.size () + 1);
+		i.Write ((uint8_t*) m_optionGateway.c_str (), m_optionGateway.size () + 1);
+		// DNS server's IP address
+	  i.WriteU16 (m_optionDNS.size () + 1);
+		i.Write ((uint8_t*) m_optionDNS.c_str (), m_optionDNS.size () + 1);
+		// DNS server 2 IP address
+	  i.WriteU16 (m_optionDNS2.size () + 1);
+		i.Write ((uint8_t*) m_optionDNS2.c_str (), m_optionDNS2.size () + 1);
+  }
+  uint32_t 
+  DHCPMessage::Deserialize (Buffer::Iterator start)
+  {
+    Buffer::Iterator i = start;
+    uint8_t receivedSize = 0;
+		m_ciaddr = ""; //!< Clients' IP address (refer the RFC)
+		m_yiaddr = ""; //!< Your (Client's) IP address 
+		m_siaddr = ""; //!< IP address of the next server to be contacted in next bootp
+		m_giaddr = ""; //!< Relay agent IP address
+		m_chaddr = ""; //!< Client's hardware Address (MAC)
+		m_optionGateway = ""; //!< Gateway address from the DHCP server
+		m_optionDNS = ""; //!< DNS server IP address
+		m_optionDNS2 = ""; //!< Secondery DNS server if awailable
+		
+		m_opcode =  i.ReadU16 (); //!< Operational Code
+		m_hType =  i.ReadU16 ();	//!< Hardware Address Type
+		m_hLen =  i.ReadU16 ();		//!< Hardware Address Length
+		m_xId =  i.ReadNtohU32 ();		//!< Transaction ID (A Random Number)
+		m_eCs =  i.ReadNtohU16 ();		//!< Time ellapsed since the request //TODO: think a method
+		m_flags =  i.ReadNtohU16 (); 	//!< Flags
+		
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		char data[receivedSize];
+		i.Read ((uint8_t*) data, receivedSize);
+		m_ciaddr = data;
+
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		data[receivedSize]={0};
+		i.Read ((uint8_t*) data, receivedSize);
+		m_yiaddr = data;
+    
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		data[receivedSize]={0};
+		i.Read ((uint8_t*) data, receivedSize);
+		m_siaddr = data;
+    
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		data[receivedSize]={0};
+		i.Read ((uint8_t*) data, receivedSize);
+		m_giaddr = data;
+		
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		data[receivedSize]={0};
+		i.Read ((uint8_t*) data, receivedSize);
+		m_chaddr = data;
+		
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		data[receivedSize]={0};
+		i.Read ((uint8_t*) data, receivedSize);
+		m_optionGateway = data;
+		
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		data[receivedSize]={0};
+		i.Read ((uint8_t*) data, receivedSize);
+		m_optionDNS = data;
+		
+		// Used for reading variable size strings
+		receivedSize = 0;
+		receivedSize = i.ReadU16 ();
+		data[receivedSize]={0};
+		i.Read ((uint8_t*) data, receivedSize);
+		m_optionDNS2 = data;
+		
+		return DHCPMessage::GetSerializedSize ();
   }	
 }// End of the Namespace
