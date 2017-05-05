66e4deda3f096632c81accbf1c6cc4686186b437
Janaka L. WIJEKOON <janaka@west.sd.keio.ac.jp>
2017-05-05 12:25:36 +0530

    with DHCPConf file

--- model/dhcp.h
+++ model/dhcp.h
@@ -14,10 +14,30 @@
 #include "ns3/net-device.h"
 #include "ns3/output-stream-wrapper.h"
 namespace ns3 {
+class DHCPBindingEntry
+{
+	/**
+		Class that is used to record the BINDING structure 
+		explained in the RFC. This class facilitates the necessary
+		reocrds for the entries.*/
+};
+std::ostream operator<< (std::ostream& os, DHCPBindingEntry const& entry);
 
-/* ... */
+class DHCPBindTable
+{
+	/**
+		Class that is working as the DINDING structure explained in the RFC.
+		*/
+};
 
+class DynamicConfigurationEntry
+{
+};
+std::ostream operator<< (std::ostream& os, DynamicConfigurationEntry const& entry);
+
+class DynamicConfigurationTable
+{
+};
 }
-
 #endif /* DHCP_H */
 
